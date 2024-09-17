777fc987374e3b7abf83b5dcda46dfb9d07b16d1
javid shabin <javid.main.act@gmail.com>
2024-09-17 07:24:12 +0530

    Install toast , axios and cofig, then created if user logined show logoned header else normal header

--- src/layout/UserLayout.jsx
+++ src/layout/UserLayout.jsx
@@ -1,19 +1,42 @@
-import React, { useState } from 'react'
-import Header from '../components/Header'
-import { Outlet } from 'react-router-dom'
-import UserHeader from '../components/user/Header'
+import React, { useEffect, useState } from 'react';
+import Header from '../components/Header';
+import { Outlet, useLocation } from 'react-router-dom';
+import UserHeader from '../components/user/Header';
+import { useDispatch, useSelector } from 'react-redux';
+import { axiosInstants } from '../config/axiosInstants';
+import { clearUser, saveUser } from '../redux/features/userSlice';
 
 const UserLayout = () => {
-    const [sample, setSample] = useState(true)
-  return (
-    <>
-    {
-        sample ? <UserHeader/> : <Header/>
+    const dispatch = useDispatch()
+    const location = useLocation()
+    const { isUserExist } = useSelector((state) => state.user); // Corrected 'isUserExists' to 'isUserExist'
+    console.log(isUserExist, "====redux"); // Now the console should show the correct value
+
+    const checkUser = async () => {
+        try {
+            await axiosInstants({
+                method: "GET",
+                url: "/user/check-user",
+            })
+            dispatch(saveUser())
+        } catch (error) {
+            dispatch(clearUser()) // If loggin error call the clear fuction false
+            console.log(error)
+        }
     }
-      
-      <Outlet />
-    </>
-  )
+
+    useEffect(()=>{
+        checkUser()
+    },[location.pathname])
+
+    return (
+      <>
+      {isUserExist ? <UserHeader /> : <Header /> }
+          
+        <Outlet />
+      </>
+    );
 }
 
-export default UserLayout
+export default UserLayout;
+
