8bd596b76f7fa52bdd8e8ad5b3e232805d39448d
javid shabin <javid.main.act@gmail.com>
2024-09-17 06:52:49 +0530

    Create user logined header

--- src/layout/UserLayout.jsx
+++ src/layout/UserLayout.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 import { Outlet } from 'react-router-dom'
+import UserHeader from '../components/user/Header'
 
 const UserLayout = () => {
+    const [sample, setSample] = useState(true)
   return (
     <>
-      <Header/>
+    {
+        sample ? <UserHeader/> : <Header/>
+    }
+      
       <Outlet />
     </>
   )
