7e87f7d3464c0ac1de1cffcd3972240b2724892d
javid shabin <javid.main.act@gmail.com>
2024-09-28 23:33:10 +0530

    A change

--- src/pages/Restaurant.jsx
+++ src/pages/Restaurant.jsx
@@ -30,7 +30,7 @@ const Restaurant = () => {
 
   if (loading) {
     return (
-      <div className="grid grid-cols-4">
+      <div className="grid grid-cols-4 mx-auto">
         <div className="flex w-52 flex-col gap-4">
           <div className="skeleton h-32 w-full"></div>
           <div className="skeleton h-4 w-28"></div>
